4439cbd0125c5207727a8f72ce1c84306abf5e99
Mina Lee <mal0070@naver.com>
2025-02-11 17:56:15 +0900

    feat: 버튼 스타일링 추가

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,18 @@
 import styles from './App.module.scss';
+import Button from '../Button/Button';
+
 
 function App() {
+  const members = ['시온', '리쿠', '유우시', '재희', '료', '사쿠야'];
 
   return (
      <div className={styles.appContainer}>
       <h1 className={styles.appContainer__title}>NCT WISH 4-CUT</h1>
-      <div className="">
-        <button type="button">시온</button>
-        <button type="button">리쿠</button>
-        <button type="button">유우시</button>
-        <button type="button">재희</button>
-        <button type="button">료</button>
-        <button type="button">사쿠야</button>
+      <div className={styles.buttonContainer}>
+        {members.map((member)=>
+          <Button key={member} text={member}/>
+        )
+        }
       </div>
      </div>
   )
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,12 @@
+import styles from './Button.module.scss';
+
+const Button = ({ text, type, onClick }) => {
+  return (
+    // biome-ignore lint/a11y/useButtonType: <explanation>
+    <button onClick={onClick} className={`${styles.Button} ${styles[`Button__${type}`]}`}>
+      {text}
+    </button>
+  );
+};
+
+export default Button;
